53df3a0e206fca2c7c9ff1e377261df3f21a7050
wangxu3 <wangxu3@yidian-inc.com>
2022-01-27 18:13:52 +0800

    Update git_script.py

--- script/git_script.py
+++ script/git_script.py
@@ -6,26 +6,30 @@ if len(sys.argv) < 2:
 	print("请使用 python3 git_script.py xxx.git")
 	print("xxx.git: 为需要下载项目的仓库地址，仅支持 HTTPS 方式 clone。")
 	sys.exit(0)
-
 # git path for clone 
 print("prepare to start clone")
-inputStr = sys.argv[1]
+gitPath = sys.argv[1]
+destPath = ""
+if len(sys.argv) == 3: 
+	destPath = sys.argv[2]
 print("start to clone main Repo")
+# Split git path to get Repo name
+preStaff = gitPath.rfind("/") + 1
+subStaff = gitPath.find(".git")
+childName = gitPath[preStaff:subStaff]
 # Generate git command
-cmd = "git clone " + inputStr
+if len(destPath) == 0:
+	destPath = os.getcwd() 
+cmd = "git clone " + gitPath + " " + destPath + "/" +childName
 print("cmd: ", cmd)
 # Execute the git command
 result = os.popen(cmd)
 print ("result" , result.read()) 
 print("main Repo completed, then start clone submodule")
 print("prepare start clone submodule")
-# Split git path to get Repo name
-preStaff = inputStr.rfind("/") + 1
-subStaff = inputStr.find(".git")
-childName = inputStr[preStaff:subStaff]
 # The current path, used to store the contents of the remote repo
-work_dir = os.getcwd() 
-repo_path = os.path.join(work_dir, childName)
+# work_dir = os.getcwd() 
+repo_path = os.path.join(destPath, childName)
 print("repo_path: ", repo_path)
 # enter the Repo
 os.chdir(repo_path)
@@ -37,3 +41,4 @@ print(cmd)
 result = os.popen(cmd)
 print ("result" , result.read()) 
 print("submodule clone completed")
+print("Repo clone finished")
