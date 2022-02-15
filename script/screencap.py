e346d061af688f387dc6572cd6bc7aee22eb6cda
wangxu3 <wangxu3@yidian-inc.com>
2022-02-15 11:11:20 +0800

    flwo

--- script/screencap.py
+++ script/screencap.py
@@ -2,11 +2,16 @@ from builtins import str
 import os
 import sys
 name = sys.argv[1]
-cmd = "adb shell screencap -p /sdcard/" + name + ".png"
-print(cmd)
+platform = sys.argv[2]
+
+if platform == "a":
+	cmd = "adb shell screencap -p /sdcard/" + name + ".png"
+	print(cmd)
+	cmd2 = "adb pull /sdcard/" + name + ".png /Users/admin/Downloads/"
+elif platform == "h":
+	cmd = "hdc shell screencap -p /sdcard/" + name + ".png"
+	print(cmd)
+	cmd2 = "hdc file recv /sdcard/" + name + ".png /Users/admin/Downloads/"
 os.popen(cmd)
-path  = os.getcwd()
-print(path)
-cmd2 = "adb pull /sdcard/" + name + ".png /Users/admin/Downloads/"
 os.popen(cmd2)
 print("report: /Users/admin/Downloads/" + name + ".png success")
